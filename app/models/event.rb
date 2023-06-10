8c8a2af95314d573265b406a78b3f84920f63216
Mayu Izumi <mayu@MayunoMacBook-Air.local>
2023-06-10 15:45:12 +0900

    バリデーション実装中

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,12 @@
 class Event < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 20}
+  validates :start_at, presence: true
+  validates :finish_at, presence: true
+  validates :memo, length: {maximum: 500}
+  validate :start_end_check
+
+  def start_end_check
+    errors.add(:finish_at, "は開始日より前の日付は登録できません。") unless
+    self.start_at < self.finish_at
+    end
 end
